2875ef25fefa00236d98a1f653d14d65e4ad0176
Robin McCollum <robin.mccollum@gmail.com>
2017-01-24 16:33:20 -0800

    Initial import of angle generated boilerplate

---
+++ tests/index.test.js
@@ -0,0 +1,20 @@
+/* global assert, setup, suite, test */
+require('aframe');
+require('../index.js');
+var entityFactory = require('./helpers').entityFactory;
+
+suite('network component', function () {
+  setup(function (done) {
+    this.el = entityFactory();
+    this.el.setAttribute('network', {});
+    this.el.addEventListener('loaded', function () {
+      done();
+    });
+  });
+
+  suite('foo property', function () {
+    test('is good', function () {
+      assert.equal(1, 1);
+    });
+  });
+});
